c5cd192e0c2cea18e1c54a4aa6c72cd091b198d0
dzyla <didny1@gmail.com>
2019-03-31 19:25:05 +0200

    Update EPU_beamtiltclasses.py

--- EPU_beamtiltclasses.py
+++ EPU_beamtiltclasses.py
@@ -3,9 +3,9 @@ import re
 '''
 Script written by Dawid Zyla, ETH Zurich
 
-This script can be used by the EPU users who didn't change names of mrc files to include the beam tilt classes into
-the Refine3D run_data.star file. The newly generated file can be imported with Relion 
-and used in the Ctfrefine job which will recognise detected classes
+This script can be used by the EPU users who didn't change names of mrc (mrcs) files. Script adds the particle beam tilt 
+classes into the Refine3D run_data.star file extracted from the EPU recorded mrc file names. The newly generated file 
+can be imported with Relion and used in the Ctfrefine job that should recognise detected classes
 
 Check if your EPU collected files have a name similar to:
 
